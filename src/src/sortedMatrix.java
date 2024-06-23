67fb61b684cf56662331416eda98a89a7c6561b4
lewyins <lightslayer135@gmail.com>
2024-06-23 22:05:24 +0545

    added 2D matrix solved using binary search

--- src/src/sortedMatrix.java
+++ src/src/sortedMatrix.java
@@ -1,5 +1,7 @@
 package src;
 
+import sun.util.resources.cldr.ext.CurrencyNames_pcm;
+
 public class sortedMatrix {
     //convert into 1D array and apply binary Search
     public static void main(String[] args) {
@@ -27,6 +29,34 @@ public class sortedMatrix {
         int col = matrix[0].length;
         if (row == 1) {
             return binarySearch(matrix, 0, 0, col - 1, target);
+        }
+        int rowStart = 0;
+        int rowEnd = row - 1;
+        int cMid = col / 2;
+        while (rowStart < (rowEnd - 1)) {
+            int mid = rowStart + (rowEnd - rowStart) / 2;
+            if (matrix[mid][cMid] == target) {
+                return new int[]{mid, cMid};
+            }
+            if (matrix[mid][cMid] < target) {
+                rowStart = mid;
+            } else {
+                rowEnd = mid;
+            }
+        }
+        if (matrix[rowStart][cMid] == target) {
+            return new int[]{rowStart + 1, cMid}
+        }
+        if (target <= matrix[cMid - 1][rowStart]) {
+            binarySearch(matrix, rowStart, 0, cMid - 1, target)
+        }
+        if (target >= matrix[cMid + 1][rowStart] && target <= matrix[rowStart][col - 1]) {
+            binarySearch(matrix, rowStart, cMid + 1, col - 1, target)
+        }
+        if (target <= matrix[rowStart + 1][cMid - 1]) {
+            return binarySearch(matrix, rowStart + 1, 0, cMid - 1, target)
+        } else {
+            return binarySearch(matrix, rowStart + 1, cMid+1, cMid - 1, target)
         }
         //run the loop till 2 rows are remaining
         return new int[]{-1, -1};
